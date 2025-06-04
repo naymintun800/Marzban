8ece721dcd2f335de713f4747a4fbd81f528c800
Naiko <silent.slsv@gmail.com>
2025-06-04 11:51:10 +0000

    patch: generate 0a587bdb4f4f_add_load_balancer_related_tables.py

---
+++ app/db/migrations/versions/0a587bdb4f4f_add_load_balancer_related_tables.py
@@ -0,0 +1,24 @@
+"""add_load_balancer_related_tables
+
+Revision ID: 0a587bdb4f4f
+Revises: add_custom_subscription_fields
+Create Date: 2025-06-04 11:48:11.197507
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0a587bdb4f4f'
+down_revision = 'add_custom_subscription_fields'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
